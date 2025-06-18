469098acfcea827673b3df00d3c224feaaa7c515
Sanghyeon <sanghyeon@snu.ac.kr>
2025-06-18 16:13:47 +0900

    Initial

--- src/coreclr/jit/lowerriscv64.cpp
+++ src/coreclr/jit/lowerriscv64.cpp
@@ -117,15 +117,29 @@ GenTree* Lowering::LowerMul(GenTreeOp* mul)
 {
     assert(mul->OperIsMul());
 
-    ContainCheckMul(mul);
+    // GenTree* op2 = mul->gtGetOp2();
+    // if (op2->IsCnsIntOrI())
+    // {
+    //     GenTreeIntConCommon* cns    = op2->AsIntConCommon();
+    //     ssize_t              cnsVal = cns->IconValue();
+    //     if (isPow2(cnsVal))
+    //     {
+    //         fprintf(stderr, "[CLAMP] isPow %ld\n", cnsVal);
+    //         MakeSrcContained(mul, op2);
+    //         // ContainCheckMul(mul);
 
+    //         return mul->gtNext;
+    //     }
+    // }
+
+    ContainCheckMul(mul);
     return mul->gtNext;
 }
 
 //------------------------------------------------------------------------
 // Lowering::LowerJTrue: Lowers a JTRUE node.
 //
-// Arguments:
+// Arguments: 
 //    jtrue - the JTRUE node
 //
 // Return Value:
